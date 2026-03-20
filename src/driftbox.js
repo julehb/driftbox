8048e08b49af3660e0fc05cd40f5bd972a738e76
julehb <julehb.design@gmail.com>
2026-03-20 13:22:19 +0700

    setup slider

---
+++ src/driftbox.js
@@ -0,0 +1,78 @@
+export default class Driftbox {
+    constructor(container, options = {}) {
+        this.container = container;
+        this.images = options.images || [];
+        this.current = 0;
+
+        this.init();
+    }
+
+    init() {
+        this.track = document.createElement("div");
+        this.thumb = document.createElement("div");
+
+        this.track.className = "drift-box__track";
+        this.thumb.className = "drift-box__thumb";
+
+        Object.assign(this.track.style, {
+            width: "100%",
+            height: "100%",
+            overflow: "hidden",
+            position: "relative",
+            borderRadius: "10px",
+            // cursor: "pointer",
+        });
+
+        Object.assign(this.thumb.style, {
+            display: "flex",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            height: "100%",
+            width: "100%",
+            transition: "left 0.3s ease",
+        });
+
+        this.images.forEach((src) => {
+            const img = document.createElement("img");
+            img.src = src;
+            Object.assign(img.style, {
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                flexShrink: "0",
+            });
+            this.thumb.appendChild(img);
+        });
+
+        this.track.appendChild(this.thumb);
+        this.container.appendChild(this.track);
+
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        this.track.addEventListener("click", () => {
+            this.next();
+        });
+    }
+
+    next() {
+        this.current = (this.current + 1) % this.images.length;
+        this.thumb.style.left = `-${this.current * 100}%`;
+    }
+}
+
+class DriftboxElement extends HTMLElement {
+    connectedCallback() {
+        const imagesAttr = this.getAttribute("images") || "";
+        const images = imagesAttr
+            .split(",")
+            .map((s) => s.trim())
+            .filter(Boolean);
+
+        new Driftbox(this, { images });
+    }
+}
+
+customElements.define("drift-box", DriftboxElement);
